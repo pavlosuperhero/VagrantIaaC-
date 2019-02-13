bee9fd5a1ea09692040d24b20a60b972b166079f
Ostap Kravchuk <aresall1990@gmail.com>
2019-02-14 00:09:54 +0200

    Modified vagrant configuration for DeployHost and added gitignore file

--- DeployHost/provision.sh
+++ DeployHost/provision.sh
@@ -4,6 +4,10 @@ adduser deploy
 echo "deploy:jenkins" | chpasswd
 groupadd train-schedule
 usermod -a -G train-schedule deploy
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/
+sudo sed -i 's/PasswordAuthentication no/#PasswordAuthentication no/g' /etc/ssh/
+sudo systemctl restart sshd
+sshd_config
 echo "deploy ALL=(ALL) NOPASSWD: /usr/bin/systemctl stop train-schedule" >> /etc/sudoers
 echo "deploy ALL=(ALL) NOPASSWD: /usr/bin/systemctl start train-schedule" >> /etc/sudoers
 echo -e "[Unit]\\nDescription=Train Schedule\\nAfter=network.target\\n\\n[Service]\\n\\nType=simple\\nWorkingDirectory=/opt/train-schedule\\nExecStart=/usr/bin/node bin/www\\nStandardOutput=syslog\\nStandardError=syslog\\nRestart=on-failure\\n\\n[Install]\\nWantedBy=multi-user.target" > /etc/systemd/system/train-schedule.service
@@ -11,15 +15,18 @@ echo -e "[Unit]\\nDescription=Train Schedule\\nAfter=network.target\\n\\n[Servic
 mkdir -p /opt/train-schedule
 chown root:train-schedule /opt/train-schedule
 chmod 775 /opt/train-schedule/
-yum -y install nodejs unzip
+sudo yum install -y epel-release
+sudo yum install -y nodejs
+sudo yum install -y unzip
 
-sudo yum install docker
-systemctl start docker
+sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo groupadd docker
+#sudo usermod -aG docker jenkins
 sudo usermod -aG docker deploy
 sudo systemctl restart docker
 
 #Changed ssh login allow in sshd service config
 echo "Success"
+
